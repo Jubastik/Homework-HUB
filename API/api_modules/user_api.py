c51b7fd3089f8a678781415a3ad251c681b9d48f
Artem <artem00561@gmail.com>
2022-04-13 16:41:51 +0300

    добавлена регистрация пользователя по class_token
    Took 3 hours 2 minutes

--- API/api_modules/user_api.py
+++ API/api_modules/user_api.py
@@ -1,8 +1,9 @@
 import flask
-from flask import request, jsonify, abort, make_response
+from flask import request, jsonify, make_response
 
 from API.api_modules.core import id_processing, IDError
 from API.data import db_session
+from API.data.classes import Class
 from API.data.students import Student
 
 blueprint = flask.Blueprint(
@@ -27,16 +28,24 @@ def get_user(platform, user_id):  # Возвращает базовую инфо
 def create_user():  # Создает пользователя на основе входящего Json
     # получить тело с tg_id
     if not request.json:
-        return make_response(jsonify({'error': 'Пустой json'}), 422)
+        return make_response(jsonify({'error': 'Пустой json'}), 400)
     elif not all(key in request.json for key in
                  ["platform", "id", "name"]):
         return make_response(jsonify({'error': 'Отсутствуют поля "platform", "id", "name"'}), 422)
-    db_sess = db_session.create_session()
+    db_sess = db_session.create_session()  # добавить регу по токену
     data = request.json
+    class_id = None
+    if "class_token" in data:
+        class_id = db_sess.query(Class.id).filter(Class.class_token == data["class_token"]).first()
+        if class_id is not None:
+            class_id = class_id[0]
+        else:
+            return make_response(jsonify({'error': 'Нет такого класса'}), 422)
     if data['platform'] == "tg":
         student = Student(
             tg_id=data['id'],
-            name=data['name']
+            name=data['name'],
+            class_id=class_id
         )
     else:
         return make_response(jsonify({'error': 'поддерживается только tg'}), 422)
@@ -59,7 +68,7 @@ def full_edit_user(platform, user_id):  # Полное Изменение пол
 @blueprint.route('/api/user/<platform>/<int:user_id>', methods=['PATCH'])
 def edit_user(platform, user_id):  # Изменение пользователя на основе входящего Json
     if not request.json:
-        return make_response(jsonify({'error': 'Пустой json'}), 422)
+        return make_response(jsonify({'error': 'Пустой json'}), 400)
     try:
         id = id_processing(platform, user_id)
     except IDError as e:

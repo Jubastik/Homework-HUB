4339032990b87dbc23eef5993c21f2f18487bb9d
Ru1ka <wotb5ks@gmail.com>
2022-04-24 14:40:35 +0300

    +send_error в rest/scripts

--- BOT/tgbot/services/restapi/scripts.py
+++ BOT/tgbot/services/restapi/scripts.py
@@ -1,3 +1,10 @@
+from BOT.bot import bot
+
+
 # Скрипты Rest.
 def return_error(response):
     return {"code": response.status_code, "error": response.json()["error"]}
+
+
+async def send_error(tguser_id, error_txt):
+    await bot.send_message(tguser_id, error_txt)

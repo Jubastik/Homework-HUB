3d5614dc58bee2cd7f4c3e7eaf66402b1d92a7dc
Artem <artem00561@gmail.com>
2023-01-19 20:37:48 +0300

    Исправлена проблема с регистрацией на 8:30
    Took 17 minutes

--- BOTv2/services/restapi/formatters.py
+++ BOTv2/services/restapi/formatters.py
@@ -12,13 +12,15 @@ def f_days_from_schedules(schedules):
 
 
 def create_time_tables(start_time: datetime.time):
+    start_time = datetime.combine(datetime.today(), start_time)
     time_tables = []
     for i in range(1, 9):
+        end_time = start_time + timedelta(minutes=45)
         les = {
             "lesson_number": i,
             "begin_time": start_time.strftime("%H:%M"),
-            "end_time": time(start_time.hour, start_time.minute + 45).strftime("%H:%M"),
+            "end_time": end_time.strftime("%H:%M"),
         }
         time_tables.append(les)
-        start_time = time(start_time.hour + 1, start_time.minute)
+        start_time = start_time + timedelta(minutes=60)
     return time_tables

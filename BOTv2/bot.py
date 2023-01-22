6c34c9e4628fd4cfe11e43086e78a3f3433010da
Artem <artem00561@gmail.com>
2023-01-22 15:19:33 +0300

    Реализован кэш с помощью redis
    Took 52 minutes

--- BOTv2/bot.py
+++ BOTv2/bot.py
@@ -1,18 +1,27 @@
-import os
 import locale
-import aiohttp
+import os
 
+import aiohttp
+import cloudpickle
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from tgbot.users_manager import UsersManager
 
-
 # Set's Russian locale for datetime
 locale.setlocale(locale.LC_ALL, "ru_RU.UTF-8")
 
 bot = Bot(token=os.getenv("TG_TOKEN"), parse_mode=types.ParseMode.HTML)
-bot.um = UsersManager()
+try:
+    with open("um.pcl", "rb") as f:
+        bot.um = cloudpickle.load(f)
+except FileNotFoundError:
+    print("FileNotFoundError: um.pcl")
+    bot.um = UsersManager()
+except Exception as e:
+    print("Unexpected error")
+    print(e)
+    bot.um = UsersManager()
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 headers = {"X-Requested-With": "XMLHttpRequest", "Content-Type": "application/json"}
